f509b929423c741a7f50649eb241f758757c1ab7
Kurt Lawrence <kurt@bne.precisionmining.com>
2019-03-12 16:54:45 +1000

    publish v1.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+# Change Log
+
+## v1.1.0
+
+- Added `line_iter()` function -- ability to iterate over `CategorisedSlices` as a line iterator.
+
+## v1.0.0
+
+- stable release
--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,29 @@
 //! # cansi
-//!
-//! [![Build Status](https://travis-ci.com/kurtlawrence/cansi.svg?branch=master)](https://travis-ci.com/kurtlawrence/cansi) [![Latest Version](https://img.shields.io/crates/v/cansi.svg)](https://crates.io/crates/cansi) [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cansi) [![codecov](https://codecov.io/gh/kurtlawrence/cansi/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cansi)
-//!
+//! 
+//! [![Build Status](https://travis-ci.com/kurtlawrence/cansi.svg?branch=master)](https://travis-ci.com/kurtlawrence/cansi)
+//! [![Latest Version](https://img.shields.io/crates/v/cansi.svg)](https://crates.io/crates/cansi)
+//! [![Rust Documentation](https://img.shields.io/badge/api-rustdoc-blue.svg)](https://docs.rs/cansi)
+//! [![codecov](https://codecov.io/gh/kurtlawrence/cansi/branch/master/graph/badge.svg)](https://codecov.io/gh/kurtlawrence/cansi)
+//! 
 //! ## **C**atergorise **ANSI** - ANSI escape code parser and categoriser
-//!
+//! 
 //! See the [rs docs.](https://docs.rs/cansi/)
 //! Look at progress and contribute on [github.](https://github.com/kurtlawrence/cansi)
-//!
+//! 
 //! `cansi` will parse text with ANSI escape sequences in it and return a deconstructed text with metadata around the colouring and styling. `cansi` is only concerned with `CSI` sequences, particuarly the `SGR` parameters. `cansi` will not construct escaped text, there are crates such as [`colored`](https://crates.io/crates/colored) that do a great job of colouring and styling text.
-//!
+//! 
 //! ## Example usage
-//!
+//! 
 //! > This example was done using the `colored` crate to help with constructing the escaped text string. It will work with other tools that inject escape sequences into text strings (given they follow [ANSI specification](https://en.wikipedia.org/wiki/ANSI_escape_code)).
-//!
+//! 
 //! ```rust
 //! extern crate cansi;
 //! extern crate colored;
-//!
+//! 
 //! use cansi::*;
 //! use colored::*;
 //! use std::io::Write;
-//!
+//! 
 //! let v = &mut Vec::new();
 //! write!(
 //!   v,
@@ -33,14 +36,14 @@
 //!   "!".bright_red().on_bright_yellow(),
 //! )
 //! .unwrap();
-//!
+//! 
 //! let text = String::from_utf8_lossy(&v);
 //! let result = categorise_text(&text); // cansi function
-//!
+//! 
 //! assert_eq!(result.len(), 7); // there should be seven differently styled components
-//!
+//! 
 //! assert_eq!("Hello, world!", &construct_text_no_codes(&result));
-//!
+//! 
 //! // 'Hello, ' is just defaults
 //! assert_eq!(
 //!   result[0],
@@ -57,7 +60,7 @@
 //!     strikethrough: false
 //!   }
 //! );
-//!
+//! 
 //! // 'w' is coloured differently
 //! assert_eq!(
 //!   result[1],
